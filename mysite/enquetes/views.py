c2be38e8e08e3fd676aedc46ef1d5f49586d84f4
YasmimRapos0 <yasmim.raposo@escolar.ifrn.edu.br>
2024-04-06 01:45:06 +0000

    Criação da Classe de Modelo e Cadastramento de Instâncias

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Pergunta
+
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Aplicação de Enquetes - DSWeb 2024.1: <br> Disciplina: Desenvolvimento de Sistemas Web <br> Semestre: 2024.1 <br> Matrícula do aluno: 20231014040007 <br> Nome do discente: Yasmim Raposo')
+    enquetes =  Pergunta.objects.order_by('-data_pub')[:10]
+    contexto = {'lista_enquetes': enquetes}
+    return render(request, 'enquetes/index.html', contexto)
+
+def detalhes(request, pergunta_id):
+
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = {'enquete': pergunta}
+    return render(request, 'enquetes/detalhes.html', contexto)
+
+def resultado(request, pergunta_id):
+    resultado = "RESULTADO da enquete de número %s."
+    return HttpResponse(resultado  % pergunta_id)
+
+def votacao(request, pergunta_id):
+    resultado = "VOTACAO da enquete de número %s."
+    return HttpResponse(resultado  % pergunta_id)
+
+
 
 
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['YasmimRapos0.pythonanywhere.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'raiz.apps.RaizConfig',
+    'enquetes.apps.EnquetesConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
