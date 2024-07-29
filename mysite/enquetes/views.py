c8afddba7b241448019e0e27ae93f0bcbf6d7aee
YasmimRaposo <149025189+YasmimRaposo@users.noreply.github.com>
2024-07-29 14:52:37 -0300

    Update views.py

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,32 +1,59 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Pergunta
-
-
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Pergunta, Alternativa
 
 def index(request):
-    enquetes =  Pergunta.objects.order_by('-data_pub')[:10]
+    enquetes = Pergunta.objects.order_by('-data_pub')[:10]
     contexto = {'lista_enquetes': enquetes}
     return render(request, 'enquetes/index.html', contexto)
 
 def detalhes(request, pergunta_id):
-
-    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
     contexto = {'enquete': pergunta}
-    return render(request, 'enquetes/detalhes.html', contexto)
+    return render (request, 'enquetes/detalhes.html',contexto)
 
-def resultado(request, pergunta_id):
-    resultado = "RESULTADO da enquete de número %s."
-    return HttpResponse(resultado  % pergunta_id)
 
 def votacao(request, pergunta_id):
-    resultado = "VOTACAO da enquete de número %s."
-    return HttpResponse(resultado  % pergunta_id)
+    pergunta = get_object_or_404(Pergunta,pk=pergunta_id)
+    try:
+        id_alternativa = request.POST['escolha']
+        alt = pergunta.alternativa_set.get(pk=id_alternativa)
+    except (KeyError, Alternativa.DoesNotExist):
+        contexto={
+        'enquete': pergunta,
+        'error' :'você precisa selecionar uma alternativa.'
+        }
+        return render(request,'enquetes/detalhes.html', contexto)
+    else:
+        alt.quant_votos += 1
+        alt.save()
+        return HttpResponseRedirect(reverse(
+            'enquetes:resultado', args=(pergunta.id,)
+        ))
+
+    resultado = 'VOTAÇÃO da enquete de número %s'
+    return HttpResponse(resultado % pergunta_id)
 
 
+def resultado(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    contexto = {'enquete': pergunta}
+    return render (request, 'enquetes/resultado.html',contexto)
 
+####
+## Histórico de Versões
+"""
+--> View INDEX - Versão 1
+def index(request):
+    enquetes = Pergunta.objects.all()
+    template = loader.get_template('enquetes/index.html')
+    contexto = {'lista_enquetes': enquetes}
+    return HttpResponse(template.render(contexto, request))
 
+--> View DETALHES - Versão 1
+def detalhes(request, pergunta_id):
+    resultado = 'DETALHES da enquete de número %s'
+    return HttpResponse(resultado % pergunta_id)
 
-
+"""
